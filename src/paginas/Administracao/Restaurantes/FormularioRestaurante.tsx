abcfd29a2f45aaa10f949fc8b2092ff7645f1428
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-05 03:20:29 -0300

    Created a page to add a restaurant name into the api

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,10 +1,34 @@
 import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
 
 const FormularioRestaurante = () => {
+	const [nomeRestaurante, setNomeRestaurante] = useState("");
+
+	const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+		evento.preventDefault();
+
+		axios
+			.post("http://localhost:8000/api/v2/restaurantes/", {
+				nome: nomeRestaurante,
+			})
+			.then(() => {
+				alert("Restaurante cadastrado com sucesso");
+			});
+	};
+
 	return (
-		<form>
-			<TextField id="standard-basic" label="Standard" variant="standard" />
-			<Button variant="outlined">Outlined</Button>
+		<form onSubmit={aoSubmeterForm}>
+			<TextField
+				value={nomeRestaurante}
+				onChange={evento => setNomeRestaurante(evento.target.value)}
+				id="standard-basic"
+				label="Nome do Restaurante"
+				variant="standard"
+			/>
+			<Button type="submit" variant="outlined">
+				Salvar
+			</Button>
 		</form>
 	);
 };
