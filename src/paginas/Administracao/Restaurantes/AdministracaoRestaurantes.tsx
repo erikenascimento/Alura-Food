0aea86d9edc3da2b2a636f5726c0b8a3ae037c51
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-06 19:30:16 -0300

    Created a edit button in the table container redirecting to the administrative restaurant page

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -10,6 +10,7 @@ import {
 	TableRow,
 } from "@mui/material";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
 	const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -25,12 +26,18 @@ const AdministracaoRestaurantes = () => {
 				<TableHead>
 					<TableRow>
 						<TableCell>Nome</TableCell>
+						<TableCell>Editar</TableCell>
 					</TableRow>
 				</TableHead>
 				<TableBody>
 					{restaurantes.map(restaurante => (
 						<TableRow key={restaurante.id}>
 							<TableCell>{restaurante.nome}</TableCell>
+							<TableCell>
+								[
+								<Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>
+								]
+							</TableCell>
 						</TableRow>
 					))}
 				</TableBody>
