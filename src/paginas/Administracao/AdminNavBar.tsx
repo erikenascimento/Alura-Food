7a2d405f6080266400651eca7e6e1f3d160d9720
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-07 23:23:25 -0300

    Adding the link to the admin dishes page into the navbar interface

--- src/paginas/Administracao/AdminNavBar.tsx
+++ src/paginas/Administracao/AdminNavBar.tsx
@@ -31,6 +31,10 @@ const AdminNavBar = () => {
 							<Link component={RouterLink} to="/admin/pratos">
 								<Button sx={{ my: 2, color: "white" }}>Pratos</Button>
 							</Link>
+
+							<Link component={RouterLink} to="/admin/pratos/novo">
+								<Button sx={{ my: 2, color: "white" }}>Novo Prato</Button>
+							</Link>
 						</Box>
 					</Toolbar>
 				</Container>
