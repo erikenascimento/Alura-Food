fca3b3af03b961c5ca41dddf27f003d879806e48
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-08 01:37:59 -0300

    Using a form data to define teh structure of the json document who will be sent to the backend API

--- src/paginas/Administracao/Pratos/FormularioPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -38,6 +38,34 @@ const FormularioPrato = () => {
 
 	const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
 		evento.preventDefault();
+
+		const formData = new FormData();
+
+		formData.append("nome", nomePrato);
+		formData.append("descricao", descricao);
+		formData.append("tag", tag);
+		formData.append("restaurante", restaurante);
+		if (imagem) {
+			formData.append("imagem", imagem);
+		}
+
+		axiosBaseURL
+			.request({
+				url: "pratos/",
+				method: "POST",
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+				data: formData,
+			})
+			.then(() => {
+				setNomePrato("");
+				setDescricao("");
+				setTag("");
+				setRestaurante("");
+				alert("Prato cadastrado com sucesso");
+			})
+			.catch(error => console.log(error));
 	};
 
 	const selecionarArquivo = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -98,7 +126,7 @@ const FormularioPrato = () => {
 									onChange={evento => setTag(evento.target.value)}
 								>
 									{tags.map(tag => (
-										<MenuItem key={tag.id} value={tag.id}>
+										<MenuItem key={tag.id} value={tag.value}>
 											{tag.value}
 										</MenuItem>
 									))}
